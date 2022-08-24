d5b95fe63fe75e0791a7210b619911a6dc7ac9cc
Darlene Matos <darlenem19@gmail.com>
2022-08-24 11:59:31 -0400

    Create login endpoint

--- server/routes/users.js
+++ server/routes/users.js
@@ -28,6 +28,9 @@ router.post('/register', (req, res) => {
     if(!isValid) {
         return res.status(400).json(errors)
     }
+
+
+//find user by eamil
 // check to see if user already exists 
     User.findOne( {email: req.body.email}).then(user => {
 
@@ -42,7 +45,7 @@ router.post('/register', (req, res) => {
                 churchName: req.body.churchName
             });
 
-    //Has password before saving in database
+    //Hash password before saving in database
 
             bcrypt.genSalt(10, (err, salt) => {
                 bcrypt.hash(newUser.password, salt, (err, hash) =>{
@@ -57,3 +60,66 @@ router.post('/register', (req, res) => {
         };
     });
 });
+
+
+router.post('/login', (req,res) => {
+    //form validation 
+
+    const {errors, isValid} = validateLoginInput(req.body);
+
+    //Check validation
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //Find user by email
+
+    User.findOne({email}).then(user => {
+        // Check if user exists 
+
+        if(!user) {
+            return res.status(404).json({emailnotfound: "Email not found"
+        });
+        }
+
+    //Check password 
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if(isMatch) {
+                //User Matched
+                //Create JWT Payload
+
+                const payload = {
+                    id: user.id,
+                    name: user.name
+                };
+
+            //Sign token
+
+            jwt.sign(
+                payload,
+                keys.secretOrKey,
+                {
+                    expiresIn: 31556926 // 1 year in seconds
+                },
+                (err, token) => {
+                    res.json( {
+                        success: true,
+                        token: "Bearer" + token
+                    });
+                }
+            );
+            } else {
+                return res
+                .status(400)
+                .json({passwordincorrect: "Password is incorrect"});
+            }
+        });
+    });
+
+
+});
+
+module.exports = router;
