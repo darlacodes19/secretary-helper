fe4e2962b1e184643461110b8bf37ff2856482cc
Darlene Matos <darlenem19@gmail.com>
2022-08-25 15:34:25 -0400

    Create logic for CRUD operations

--- server/controllers/projectBoardControllers.js
+++ server/controllers/projectBoardControllers.js
@@ -1,20 +1,37 @@
 // Projects CRUD logic 
-const project = require('../db/models/project');
+const Project = require('../db/models/project');
+
 
 //@desc get all projects
 //@route GET 
 //@access Private
 
-const getAllProjects= (req, res) => {
+const getAllProjects = async (req, res) => {
+     try{
+        const projects = await Project.find()
+        return res.status(200).json({projects})
 
+     }catch(error) {
+        return res.status(500).send(error.message);
+     }
 }
 
 //@desc get a specific project
 //@route GET 
 //@access Private
 
-const getOneProject= (req, res) => {
+const getOneProject= async (req, res) => {
+    try {
+        const {id} = req.params;
+        const project = await Project.findById(id)
+        if(project ){
+            return res.status(200).json({project})
+        } return res.status(404).sned('Project not found')
+        
 
+    }catch(error){
+        return res.status(500).send(error.message)
+    }
 }
 
 
@@ -23,7 +40,17 @@ const getOneProject= (req, res) => {
 //@access Private
 
 const createProject = async (req, res) => {
-    
+    try{
+
+        const project = new Project(req.body)
+        await project.save()
+        return res.status(201).json({
+            project,
+        })
+
+    }catch(error) {
+        return res.status(500).json({error:error.message})
+    }
 }
 
 
@@ -32,7 +59,17 @@ const createProject = async (req, res) => {
 //@access Private
 
 const deleteProject = async (req, res) => {
-  
+  try {
+    const {id} = req.params
+    const deleted = await Project.findByIdAndDelete(id)
+    if(deleted){
+        return res.status(200).send('Project deleted')
+    }
+    throw new error ('Project not found')
+
+  }catch(error){
+    return res.status(500).send(error.message)
+  }
 }
 
 //@desc edit member information  (UPDATE)
@@ -40,6 +77,20 @@ const deleteProject = async (req, res) => {
 //@access Private
 
 const updateProject = (req, res) => {
+    try{
+
+        const {id} = req.params;
+        Project.findByIdAndUpdate(id, req.body, {new: true}, {err, Project})
+
+        if(error) {
+            res.status(500).sned(err)
+        }
+        if (!Project) {
+            res.status(500).send('Project not found')
+        }
+    }catch(error){
+        return res.status(500).send(error.message)
+    }
 
 }
 
