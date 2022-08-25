56a86c2868487b7a026b766e6cf8b2c236dec051
Darlene Matos <darlenem19@gmail.com>
2022-08-25 13:47:13 -0400

    Create the logic for each CRUD operation

--- server/controllers/memberControllers.js
+++ server/controllers/memberControllers.js
@@ -1,20 +1,38 @@
 // members CRUD logic 
-const member = require('../db/models/member');
+const Member = require('../db/models/member');
 
 //@desc get all memebrs
 //@route GET 
 //@access Private
 
-const getMember = (req, res) => {
+const getMembers = async (req, res) => {
+    
+    try {
+        const members = await Member.find()
+        return res.status(200).json({members})
 
+    }catch(error) {
+        return res.status(500).send(error.message)
+    }
 }
 
 //@desc get a specifict member
 //@route GET 
 //@access Private
 
-const getOneMember= (req, res) => {
+const getOneMember = async (req, res) => {
+    try{
 
+        const {id} = req.params;
+        const member =  await Item.findById(id)
+        if(member){
+            return res.status(200).json({member})
+        }
+        return res.status(404).send('member not found')
+
+    }catch(error){
+        return res.status(500).send(error.message)
+    }
 }
 
 
@@ -23,7 +41,14 @@ const getOneMember= (req, res) => {
 //@access Private
 
 const createMember = async (req, res) => {
-    
+    try{
+        const member = new Member(req.body)
+        await member.save()
+        return res.status(201).json({member})
+
+    }catch(error) {
+        return res.status(500).send(error.message)
+    }
 }
 
 
@@ -32,7 +57,16 @@ const createMember = async (req, res) => {
 //@access Private
 
 const deleteMember = async (req, res) => {
-  
+  try{
+
+    const {id} = req.params;
+    const deleted = await Item.findByIdAndDelete(id)
+    if (deleted) {
+        return res.status(200).send('Member deleted')
+    }
+  }catch(error){
+    return res.status(500).send(error.message)
+  }
 }
 
 //@desc edit member information  (UPDATE)
@@ -40,14 +74,28 @@ const deleteMember = async (req, res) => {
 //@access Private
 
 const updateMember = (req, res) => {
+    try {
+        const {id} = req.params
+        Member.findByIdAndUpdate(id, req.body, {new: true}, {err, Member})
 
+        if (error) {
+            res.status(500).send(err)
+        }
+        if(!item) {
+            res.status(500).send('Member not found')
+            
+        }
+        return res.status(200).json(item)
+    }catch(error){
+        return res.status(500).send(error.message)
+    }
 }
 
 
 module.exports= {
 
     updateMember,
-    getMember,
+    getMembers,
     getOneMember,
     createMember,
     deleteMember,
