b13ac09d93d4982e609a1338dba8173be92e2a96
Darlene Matos <darlenem19@gmail.com>
2022-08-25 11:51:19 -0400

    Fix validation typo error

--- server/validation/register.js
+++ server/validation/register.js
@@ -2,13 +2,15 @@ const Validator = require('validator');
 const isEmpty = require('is-empty');
 
 module.exports = function validateRegisterInput(data) {
+
+    let errors = {};
     //Convert empty fields to an empty string so we can use validator functions
 
-    data.name = !isEmpty(data.name) ? data.name : "";
-    data.email = !isEmpty(data.email) ? data.email : "";
-    data.churchName = !isEmpty(data.churchName) ? data.churchName : "";
-    data.password = !isEmpty(data.password) ? data.password : "";
-    data,password2 = !isEmpty(data.password2) ? data.password2 : "";
+    data.name = !isEmpty(data.name) ? data.name : " ";
+    data.email = !isEmpty(data.email) ? data.email : " ";
+    data.churchName = !isEmpty(data.churchName) ? data.churchName : " ";
+    data.password = !isEmpty(data.password) ? data.password : " ";
+    data.password2 = !isEmpty(data.password2) ? data.password2 : " ";
     
     //Name checks
     if(Validator.isEmpty(data.name)) {
@@ -17,7 +19,7 @@ module.exports = function validateRegisterInput(data) {
     //Email checks 
     if(Validator.isEmpty(data.email)) {
         errors.email = "Email field is required";
-    }else if (!validator.isEmail(data.email)){
+    }else if (!Validator.isEmail(data.email)){
         errors.email = "Email is invalid";
     }
 
@@ -28,19 +30,19 @@ module.exports = function validateRegisterInput(data) {
 
     //Password checks
     if(Validator.isEmpty(data.password)) {
-        erros.password = "Password field is requried";
+        errors.password = "Password field is requried";
     }
 
     if(Validator.isEmpty(data.password2)){
-        erros.password2 = "Confirm passord field is required";
+        errors.password2 = "Confirm passord field is required";
     }
 
-    if(!validator.equals(data.passwod, {min: 6, max:30})) {
+    if(!Validator.isLength(data.password, {min: 6, max:30})) {
         errors.password = "password must be at least 6 characters";
     }
     //passwords must match
 
-    if(!validator.equals(data.password , data.password2)){
+    if(!Validator.equals(data.password , data.password2)){
         errors.password2 = "Passwords must match"
     }
 
