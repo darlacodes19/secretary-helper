0e44175c6a9d2d51ab33c37ef49a5d5ef291f951
Darlene Matos <darlenem19@gmail.com>
2022-08-25 21:16:21 -0400

    Create navigation component

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+const Navigation = () => {
+    return (
+        <div className="landing-page-header">
+            <ul className= 'nav-links'>
+                <li className="nav-item"> Logo </li>
+                <li className="nav-item"> <a href='#'>  Sign In </a> </li> 
+                <li className="nav-item"> <button className="nav-button"> Sign Up </button> </li> 
+            </ul>
+        </div>
+    )
+}
+
+export default Navigation 
