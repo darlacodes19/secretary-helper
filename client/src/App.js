01619814947551e57ab996dc69f68094a69f0cdf
Darlene Matos <darlenem19@gmail.com>
2022-08-26 13:17:36 -0400

    Add routes to the website

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
+  <Router> 
     <div className="App">
-      <LandingPage/>
+      
+      <Routes> 
+        <Route path='/' element={<LandingPage />} /> 
+        <Route path='/login' element={<Login /> } /> 
+        <Route path='/register' element={<Register /> } /> 
+
+      </Routes> 
     </div>
+  </Router> 
   );
 }
 
