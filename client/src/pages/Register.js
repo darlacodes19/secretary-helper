d652bf82dc5c631d78104942287208d445dcc73d
Darlene Matos <darlenem19@gmail.com>
2022-08-27 10:52:20 -0400

    Modify page structure

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,67 +1,94 @@
-import {useState} from 'react'
-import Logo from '../components/Logo'
-import axios from 'react'
+import { useState } from 'react'
+import FormSuccess from '../components/FormSuccess'
+import RegistrationForm from '../components/RegistrationForm'
+
 function Register () {
 
-    const [name, setName] =  useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [churchName, setChurchName] = useState('')
+    const[isSubmitted, setIsSubmitted] = useState(false)
 
-    async function registerUser(event) {
-        event.preventDefault()
-       await axios('http://localhost:3001/routes/users/register' ,{
-            method: 'POST',
-            headers: {
-                'Contet-Type': 'application/json'
-
-            },
-            body: JSON.stringify ({
-                name,
-                email,
-                password,
-                churchName
-            }),
-        })
-
-        const data = await Response.json()
-        console.log(data)
+    function submitForm() {
+        setIsSubmitted(true)
     }
 
+    // const [name, setName] =  useState('')
+    // const [email, setEmail] = useState('')
+    // const [password, setPassword] = useState('')
+    // const [churchName, setChurchName] = useState('')
+    // const [password2, setPassword2] = useState('')
+    
+
+    // async function registerUser(event) {
+    //     event.preventDefault()
+    //    await axios('http://localhost:3001/routes/users/register' ,{
+    //         method: 'POST',
+    //         headers: {
+    //             'Contet-Type': 'application/json'
+
+    //         },
+    //         body: JSON.stringify ({
+    //             name,
+    //             email,
+    //             password,
+    //             churchName
+    //         }),
+    //     })
+
+    //     const data = await Response.json()
+    //     console.log(data)
+  
+
     return (
+        <div>
+
+           {!isSubmitted ? (<RegistrationForm submitForm={submitForm}/>) : (<FormSuccess />)}
+        
+            
+        </div> 
+
+    )
 
         
-        <div> 
-            <Logo /> 
-            <h1> Creat an Account</h1>
-            <form onSubit ={registerUser}> 
-                <input 
-                type="text" 
-                placeholder="Full Name" 
-                value={name}
-                />
-                <input 
-                type="text" 
-                placeholder="Church Name" 
-                value={churchName}
-                />
-                <input 
-                type="email" 
-                placeholder="Email" 
-                value={email}
-                />
-                <input 
-                type="password" 
-                placeholder="password" 
-                value={password}
-                />
-                <input 
-                type = "submit" 
-                value="register" 
-                /> 
-            </form>
-        </div>
-    )
+    //     <div className='registration-page'> 
+    //         <Logo /> 
+    //         <div className='registration-form-container'> 
+    //         <img src= {require('../assets/SH2.png')} alt='secretary' className='image'/>
+    //         <div className='form-container'> 
+    //             <h1> Creat an Account</h1>
+    //             <form onSubit ={registerUser} className="registration-form"> 
+    //                 <input 
+    //                 type="text" 
+    //                 placeholder="Full Name" 
+    //                 value={name}
+    //                 />
+    //                 <input 
+    //                 type="text" 
+    //                 placeholder="Church Name" 
+    //                 value={churchName}
+    //                 />
+    //                 <input 
+    //                 type="email" 
+    //                 placeholder="Email" 
+    //                 value={email}
+    //                 />
+    //                 <input 
+    //                 type="password" 
+    //                 placeholder="password" 
+    //                 value={password}
+    //                 />
+    //                 <input 
+    //                 type="password2" 
+    //                 placeholder="Write password again" 
+    //                 value={password2}
+    //                 />
+    //                 <input 
+    //                 type = "submit" 
+    //                 value="SignUp" 
+    //                 /> 
+    //             </form>
+    //         </div> 
+    //         </div> 
+    //     </div>
+    // )
 }
 
 export default Register
