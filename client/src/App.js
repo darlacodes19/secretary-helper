19f125308e200e7b95b1a7e0a200c9dba8a92f56
Darlene Matos <darlenem19@gmail.com>
2022-08-28 15:35:11 -0400

    Add memebrs page and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Members from './pages/Members';
 
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path='/' element={<LandingPage />} /> 
         <Route path='/login' element={<Login /> } /> 
         <Route path='/register' element={<Register /> } /> 
+        <Route path={'/dashboard'} element ={<Dashboard />} />
+        <Route path ={'/members'} element = {<Members />} /> 
 
       </Routes> 
     </div>
