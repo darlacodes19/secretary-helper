9d3085b42acb47de95a3285dbee901df2a89fd19
Darlene Matos <darlenem19@gmail.com>
2022-08-30 10:47:06 -0400

    Add logout button

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from './Logo'
+
+export default function Header() {
+  return (
+
+    <div className='header'>
+        <Logo /> 
+        <button> LogOut </button> 
+       
+    </div>
+  )
+}
