0b32d01700a8f68d9116b641d79f7f542415f4b7
Darlene Matos <darlenem19@gmail.com>
2022-08-30 10:50:37 -0400

    Add google material symbols to form

--- client/src/components/RegistrationForm.jsx
+++ client/src/components/RegistrationForm.jsx
@@ -11,83 +11,75 @@ const RegistrationForm = ({submitForm}) => {
   return (
    
     <div className='form-content-right'>
-      <form className="registration-from" onSubmit={handleSubmit}>
-        <h2> Create an account.</h2>
+      <form className="registration-form" onSubmit={handleSubmit}>
+        <h2 className='form-header'> Register </h2>
+        <p> Create your account. Its free and only takes a minute </p> 
         <span> Already have an account? <NavLink to={'/login'} > Login Here </NavLink></span> 
+      <div className='form' >
         <div className="form-inputs">
-            <label className='form-inputs' htmlFor='Name'> 
-               Full name 
-            </label>
-            <input 
-                type="text" 
-                name="name" 
-                className='form-input'
-                placeholder='Enter your full name'
-                value={values.name}
-                onChange={handleChange}
-                 /> 
-                 {errors.name && <p> {errors.name}</p>}
+          <span class="material-symbols-outlined" > person </span>
+              <input 
+                  type="text" 
+                  name="name" 
+                  className='form-input'
+                  placeholder='Full Name'
+                  value={values.name}
+                  onChange={handleChange}
+                  /> 
+                  {errors.name && <p> {errors.name}</p>}
         </div>
         <div className="form-inputs">
-            <label className='form-inputs' htmlFor='church name'> 
-               Church Name
-            </label>
-            <input 
-                type="text" 
-                name="church name" 
-                className='form-input'
-                placeholder='Enter your church name'
-                value={values.churchName}
-                onChange={handleChange}
-                 /> 
-                   {errors.churchName && <p> {errors.churchName}</p>}
+          <span class="material-symbols-outlined"> church </span>
+              <input 
+                  type="text" 
+                  name="church name" 
+                  className='form-input'
+                  placeholder='Church Name'
+                  value={values.churchName}
+                  onChange={handleChange}
+                  /> 
+                    {errors.churchName && <p> {errors.churchName}</p>}
         </div>
         <div className="form-inputs">
-            <label className='form-inputs' htmlFor='church name'> 
-               Email Address
-            </label>
-            <input 
-                type="email" 
-                name="email" 
-                className='form-input'
-                placeholder='Enter your email'
-                value={values.email}
-                onChange={handleChange}
-                 /> 
-                   {errors.email && <p> {errors.email}</p>}
+          <span class="material-symbols-outlined"> mail </span>
+              <input 
+                  type="email" 
+                  name="email" 
+                  className='form-input'
+                  placeholder='Email'
+                  value={values.email}
+                  onChange={handleChange}
+                  /> 
+                    {errors.email && <p> {errors.email}</p>}
         </div>
         <div className="form-inputs">
-            <label className='form-inputs' htmlFor='password'> 
-               Passwrod
-            </label>
-            <input 
-                type="password" 
-                name="password" 
-                className='form-input'
-                placeholder='Create a password'
-                value={values.password}
-                onChange={handleChange}
-                 /> 
-                   {errors.password && <p> {errors.password}</p>}
+          <span class="material-symbols-outlined"> lock </span>
+              <input 
+                  type="password" 
+                  name="password" 
+                  className='form-input'
+                  placeholder='Password'
+                  value={values.password}
+                  onChange={handleChange}
+                  /> 
+                    {errors.password && <p> {errors.password}</p>}
         </div>
         <div className="form-inputs">
-            <label className='form-inputs' htmlFor='passwrod2'> 
-               Confirm password
-            </label>
-            <input 
-                type="password2" 
-                name="password2" 
-                className='form-input'
-                placeholder='Enter your password'
-                value={values.password2}
-                onChange={handleChange}
-                 /> 
-                  {errors.password2 && <p> {errors.password2}</p>}
+        <span class="material-symbols-outlined"> lock </span>
+              <input 
+                  type="password2" 
+                  name="password2" 
+                  className='form-input'
+                  placeholder='Confirm Password'
+                  value={values.password2}
+                  onChange={handleChange}
+                  /> 
+                    {errors.password2 && <p> {errors.password2}</p>}
         </div>
-        <button className="form-input-btn" type='submit'> SignUp </button>
+        </div>
+        <button className="form-button" type='submit'> SignUp </button>
+      </form>  
         
-        
-        </form>  
        
     </div>
   )
