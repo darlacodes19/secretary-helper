9742460ecde6f7995e27e2f8298bf48743b4b22c
Darlene Matos <darlenem19@gmail.com>
2022-08-30 10:33:00 -0400

    Add dashboard page

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,20 @@
+import SideBar from '../components/SideBar'
+import Header from '../components/Header'
+import DashboardLayout from '../components/DashboardLayout'
+
+
+
+
+
+export default function Dashboard() {
+  return (
+
+    <div>
+        <Header /> 
+        <SideBar /> 
+        <DashboardLayout /> 
+      
+       
+    </div>
+  )
+}
