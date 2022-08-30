118ccbc36828aece4bed4dc194dc19c806bbf3a4
Darlene Matos <darlenem19@gmail.com>
2022-08-30 13:57:46 -0400

    Add structure to ToDo list

--- client/src/components/dashboard/ToDoList.jsx
+++ client/src/components/dashboard/ToDoList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios';
+import ToDoListCard from './ToDoListCard';
 
 export default function ToDoList() {
 
@@ -11,7 +12,8 @@ export default function ToDoList() {
 
     const fetchAll = async () => {
        const response = await axios.get('http://localhost:3001/routes/toDo')
-        console.log(response ,'hello')
+        console.log(response.data.items)
+        setAllTodo(response.data.items)
     }
 
     useEffect ( () => {
@@ -22,12 +24,13 @@ export default function ToDoList() {
 
     }
   return (
-    <div>
-        <h3> To Do </h3>
-        <input
-        type='text'
-        />
-        <button> Add </button>
+    <div className='to-do-container'>
+        <h3 className='to-do-header'> To Do List  </h3>
+        <button className='add-task-button'> Add New Task </button>
+
+        {allTodo.map( (task , key) => {
+            return <ToDoListCard task={task} key={key}/>
+        })}
        
 
     </div>
